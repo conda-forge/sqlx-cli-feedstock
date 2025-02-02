6b0955dec89d8e4d5313c06d0d44790ffa42b4d6
Daniel Nachun <daniel.nachun@gmail.com>
2025-02-01 18:17:35 -0800

    fix UCRT migration

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,21 +2,17 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
 export RUST_BACKTRACE=1
 export OPENSSL_DIR=$PREFIX
 
 # build
 cargo install --locked \
+    --no-track \
     --root "$PREFIX" \
     --path sqlx-cli
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/cargo-sqlx"
-"$STRIP" "$PREFIX/bin/sqlx"
 
 cargo-bundle-licenses \
     --format yaml \
     --output "${SRC_DIR}/THIRDPARTY.yml"
-
-# remove extra build files
-rm -f "${PREFIX}/.crates2.json" "${PREFIX}/.crates.toml"
